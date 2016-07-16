9e5bca924ed7b5e46d2474b3dd75a46cc1995d36
Jason Wong <jaw.wong@mail.utoronto.ca>
2016-07-16 16:58:44 -0400

    Initial commit with configuration file in tests branch

---
+++ calendar_service_test.js
@@ -0,0 +1,17 @@
+describe('calendar service', function(){
+	it('should generate an object containing the days of a week', function(){
+
+	})
+
+	it('shold generate an object containing the weeks of a month', function(){
+		
+	})
+
+	it('should return true if 2 dates share the same month and year', function(){
+
+	})
+
+	it('should return true if the date is today', function(){
+
+	})
+})
